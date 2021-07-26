be2f461cfb0b337c0e4537f2d5bf391536fd6b5b
Javier Acevedo <javier.acevedo@haulmer.com>
2021-07-26 10:47:38 -0400

    feat(.d.ts): add declaration module

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ export declare class Uuid extends Common {
   // define your typings manually
   // or..
   // take the ios or android .d.ts files and copy/paste them here
+
 }
 
 declare module com {
